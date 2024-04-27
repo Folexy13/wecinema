bb38dbd6e1e439c7d802a52a645131c4a350b079
Folexy13 <Folajimiopeyemisax13@gmail.com>
2024-04-27 18:30:50 +0100

    ...

--- backend/src/controller/video.js
+++ backend/src/controller/video.js
@@ -19,7 +19,7 @@ router.post("/create", async (req, res) => {
 		if (!user && author !== "admin") {
 			return res.status(404).json({ error: "Video not found" });
 		}
-
+		consoel.log(req.user);
 		// Create a new video
 		await Videos.create({
 			title,
