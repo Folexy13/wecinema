69d1b3321cd5440fb709ebe6eacfc5251dfded8a
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-06-16 01:20:30 +0500

    changess

--- frontend/src/pages/hypemodepage.tsx
+++ frontend/src/pages/hypemodepage.tsx
@@ -7,7 +7,7 @@ const hypemodepage = () => {
 			<div style={{ marginTop: 12 }} className="">
 				<div className="">
 					<img
-						src={"https://scontent.flhe3-2.fna.fbcdn.net/v/t39.30808-6/447411081_352070764649583_1013679095051761594_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=BzxUvojqo40Q7kNvgHS-yxR&_nc_ht=scontent.flhe3-2.fna&oh=00_AYDI27OiX0PQ6F23hLpEhOt2rwROIeOQ7I0dP45tNPc_sQ&oe=66634D7B"}
+						src={"https://scontent.fskt1-1.fna.fbcdn.net/v/t39.30808-6/447411081_352070764649583_1013679095051761594_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=hhGCISUc7zEQ7kNvgEcnVQB&_nc_ht=scontent.fskt1-1.fna&oh=00_AYCVJyt4T44QOXdKOR34xXAcjAcWgbYD_W2fbVOLWG_BEg&oe=6673C83B"}
 						alt="..."
 						width="1200"
 						height="0"
--- frontend/src/pages/videoeditorpage.tsx
+++ frontend/src/pages/videoeditorpage.tsx
@@ -8,7 +8,7 @@ const videoeditorpage = () => {
 			<div style={{ marginTop: 12 }} className="">
 				<div className="">
 					<img
-						src={"https://scontent.flhe3-1.fna.fbcdn.net/v/t39.30808-6/447443898_352078804648779_4412801656172812093_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=owicRRDN-zoQ7kNvgFq7P1r&_nc_ht=scontent.flhe3-1.fna&oh=00_AYDEQrmxKGXBVGTyNSlSas_xrKv-024JqURs17mjHz6FxA&oe=66634590"}
+						src={"https://scontent.fskt1-1.fna.fbcdn.net/v/t39.30808-6/447443898_352078804648779_4412801656172812093_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Rr2L5Hll-mQQ7kNvgE1MJQo&_nc_ht=scontent.fskt1-1.fna&oh=00_AYDZ3hKvUAGZki3CttPD6fX1WVC8KqwpYpYQ4nDjye6Rxg&oe=6673C050"}
 						alt="..."
 						width="1200"
 						height="0"
