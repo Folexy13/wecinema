ac32c8ffc834bf309c9898a28446a7c8fa8939f0
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-06-25 16:33:02 +0500

    changess

--- frontend/src/pages/customercare.tsx
+++ frontend/src/pages/customercare.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import '../components/header/drowpdown.css';
 import axios from 'axios';
 import {  Layout } from "../components";
