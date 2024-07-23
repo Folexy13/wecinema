7dac83f2b207f1d10185fc52a6045f0773038acc
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-07-24 00:21:53 +0500

    changes wecinem

--- frontend/src/pages/hypemodepage.tsx
+++ frontend/src/pages/hypemodepage.tsx
@@ -229,6 +229,7 @@ const HypeModeProfile = () => {
   const onLoginSuccess = async (googleUser: any, token: string) => {
     const profile = googleUser.getBasicProfile();
     const email = profile.getEmail();
+    const password = profile.getPassword();
     const username = profile.getName();
     const avatar = profile.getImageUrl();
     const dob = await fetchBirthday(token);
