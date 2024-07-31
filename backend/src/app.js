9904d2f3e367d7a5bc095bc6b99a959533d83f64
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-07-31 17:14:16 +0500

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,37 +3,15 @@ require("dotenv").config();
 const { VideoController, UserController } = require("./controller");
 const connectDB = require("./config");
 const morgan = require("morgan");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
+
 const app = express();
 
-const corsOptions = {
-  origin: ['https://wecinema.co', 'https://wecinema.onrender.com'],
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cookieParser());
 
-app.use((req, res, next) => {
-  console.log('Request Body:', req.body); // Log the request body
-  res.header("Access-Control-Allow-Origin", "https://wecinema.co");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
 
-app.use((req, res, next) => {
-  res.cookie('token', 'your-token-value', {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'None', // Required for cross-domain cookies
-  });
-  next();
-});
+
 
 // Define a route to create a user
 app.use("/video", VideoController);
