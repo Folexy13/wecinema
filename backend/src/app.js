985019f0d05ebe01fa304d086bdcf393c5ee0004
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-07-31 12:23:35 +0500

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,3 @@
-// app.js
-
 const express = require("express");
 require("dotenv").config();
 const { VideoController, UserController } = require("./controller");
@@ -7,33 +5,33 @@ const connectDB = require("./config");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://wecinema.co");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	next();
-  });
-  
+
+const corsOptions = {
+  origin: ['https://wecinema.co', 'https://wecinema.onrender.com'],
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 
-const corsOptions = {
-	origin: 'https://wecinema.co',
-	methods: 'GET,POST,PUT,DELETE',
-	allowedHeaders: 'Content-Type,Authorization',
-	credentials: true,
-  };
-  
-app.use(cors(corsOptions));
-// Example in Express.js
 app.use((req, res, next) => {
-	res.cookie('token', 'your-token-value', {
-	  httpOnly: true,
-	  secure: true,
-	  sameSite: 'None', // Required for cross-domain cookies
-	});
-	next();
+  res.header("Access-Control-Allow-Origin", "https://wecinema.co");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.cookie('token', 'your-token-value', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None', // Required for cross-domain cookies
   });
-  
+  next();
+});
+
 // Define a route to create a user
 app.use("/video", VideoController);
 app.use("/user", UserController);
@@ -44,5 +42,5 @@ connectDB(process.env.DB_URI);
 // Start the Express server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
