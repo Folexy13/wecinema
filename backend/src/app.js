1d1068bebeaa54a1ade63986de17d8de0fbc1dfc
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-07-31 21:02:01 +0500

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,38 @@
+// app.js
+
 const express = require("express");
 require("dotenv").config();
 const { VideoController, UserController } = require("./controller");
 const connectDB = require("./config");
 const morgan = require("morgan");
 const cors = require("cors");
-const cookieParser = require("cookie-parser"); // Add this line
 const app = express();
-
-const corsOptions = {
-  origin: ['https://wecinema.co', 'https://wecinema.onrender.com'],
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+	res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+	res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+	next();
+  });
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cookieParser()); // Add this line
 
+const corsOptions = {
+	origin: 'https://wecinema.co',
+	methods: 'GET,POST,PUT,DELETE',
+	allowedHeaders: 'Content-Type,Authorization',
+	credentials: true,
+  };
+  
+app.use(cors(corsOptions));
+// Example in Express.js
+app.use((req, res, next) => {
+	res.cookie('token', 'your-token-value', {
+	  httpOnly: true,
+	  secure: true,
+	  sameSite: 'None', // Required for cross-domain cookies
+	});
+	next();
+  });
+  
 // Define a route to create a user
 app.use("/video", VideoController);
 app.use("/user", UserController);
@@ -29,5 +43,5 @@ connectDB(process.env.DB_URI);
 // Start the Express server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
