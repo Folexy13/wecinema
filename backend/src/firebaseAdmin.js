882a74eb6248661006484c98be874ba876f21ab8
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-08-05 16:48:15 +0500

    wecinema

--- backend/src/firebaseAdmin.js
+++ backend/src/firebaseAdmin.js
@@ -5,5 +5,4 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://wecinema-5b6a4-default-rtdb.firebaseio.com/"
 });
-const db = admin.database();
 module.exports = admin;
